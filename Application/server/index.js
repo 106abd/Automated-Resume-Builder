f619548b89f653746c51e1eee5b0a7666f3d02e5
Abdullah Khan <100245127+106abd@users.noreply.github.com>
2025-02-21 11:18:37 -0500

    Initialized middleware and server dotenv file

--- Application/server/index.js
+++ Application/server/index.js
@@ -1,13 +1,19 @@
 // Module Imports
 const cors = require('cors') // Cross-Origin Resource Sharing module
+const dotenv = require('dotenv') // Dotenv (.env) processing module
 const express = require('express') //Express.JS module
 
-// Create Express app
-const app = express()
+// Initialization methods
+const app = express() // Create Express app
+dotenv.config() // Automate dotenv configurations processing
+
+// Middleware (run/use the following code everytime before a request/response process occurs)
+app.use(cors()) // Use cors to provide permissions to request origin addresses and permissible HTTP commands
+app.use(express.json()) // Automate request body data stream chunk collection and parsing to JSON notation
 
 
 // Run express app on Node.JS' built-in HTTP Module server
-const serverPort = 5000
+const serverPort = process.env.SERVER_PORT
 app.listen(serverPort, () => {
     console.log(`Server is running on port ${serverPort}`)
 })
