28f3af85b4d4c977eac258fb5e75466ca55b8792
Abdullah Khan <100245127+106abd@users.noreply.github.com>
2025-03-20 15:35:09 -0400

    Builder Navbar Logout Handler

--- Application/client/src/Components/Navbar.jsx
+++ Application/client/src/Components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-function Navbar({lastChild, lastChildPath}) {
+function Navbar({lastChild, lastChildPath, lastChildAction, lastChildActionArgs}) {
     return (
         <nav className='navbar'>
             <ul>
                 <li id='navbarTitle'><a>RESUME BUILDER</a></li>
-                <li><Link to={lastChildPath}>{lastChild}</Link></li>
+                <li><Link onClick={() => {lastChildAction(...lastChildActionArgs)}} to={lastChildPath}>{lastChild}</Link></li>
             </ul>
         </nav>
     )
--- Application/client/src/Pages/Builder.jsx
+++ Application/client/src/Pages/Builder.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import CustomBackground from '../Components/CustomBackground'
 import Navbar from '../Components/Navbar'
 import Chatbox from '../Components/Chatbox'
@@ -6,11 +7,41 @@ import Viewport from '../Components/Viewport'
 import BuilderTools from '../Components/BuilderTools'
 
 
-function Builder({authStatus, setAuthStatus}) {
+const logoutHandler = async(setAuthStatus, pageRedirect) => {
+
+    try {
+
+        const serverResponse = await fetch(`${import.meta.env.VITE_SERVER_API_ADDRESS}/users/logout`,{
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+        })
+        
+        const jsonData = await serverResponse.json()
+        console.log(jsonData)
+
+        if (serverResponse.ok) {
+            setAuthStatus(false)
+            console.log(jsonData)
+            pageRedirect('/')
+
+        } else {
+            console.error(`Server Error Logged Status Code: ${serverResponse.status}`)
+        }
+
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+
+function Builder({setAuthStatus}) {
+
+    const pageRedirect = useNavigate()
+
     return (
         <div>
             <CustomBackground />
-            <Navbar lastChild={'Logout'} lastChildPath={'/'}/>
+            <Navbar lastChild={'Logout'} lastChildAction={logoutHandler} lastChildActionArgs = {[setAuthStatus, pageRedirect]}/>
             <Chatbox />
             <Viewport />
             <BuilderTools />
