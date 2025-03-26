33f1e07701d15164b9cc92880187ac760cbd6e3c
Abdullah Khan <100245127+106abd@users.noreply.github.com>
2025-03-25 22:26:58 -0400

    Chatbox Client State Functionality and Stylings

--- Application/client/src/Components/Chatbox.jsx
+++ Application/client/src/Components/Chatbox.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, {useReducer, useState} from 'react'
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case 'ADD_CHAT_LOG':
+            return [...state, action.value]
+        case 'POP_CHAT_LOG':
+            return [...state].slice(0, -1)
+        case 'RESET_CHAT_LOG':
+            return []
+        default:
+            return state
+    }
+}
+
 
 function Chatbox() {
+
+    const [chatHistory, dispatch] = useReducer(reducer, [{id: 'user', message: 'debug'}, {id: 'system', message: 'debug'}])
+    const chatLogs = chatHistory.map((chatLog, logIndex) => <div className='chatLog' id={chatLog.id} key={logIndex}>{chatLog.message}</div>) 
+    const [userTextInput, setUserTextInput] = useState('')
+
+
+    const submissionHandler = (event) => {
+        if (event.key == "Enter" && !event.shiftKet) {
+            event.preventDefault()
+
+            if (userTextInput.trim()) {
+                dispatch({type: 'ADD_CHAT_LOG', value: {id: 'user', message: userTextInput}})
+                setUserTextInput('')
+                console.log(chatHistory)
+            }
+        }
+    }
+
+    
     return (
         <div className='boxContainer' id='chatContainer'>
-            <div className='chatBox'></div>
-            <textarea className='userInput'></textarea>
+            
+            <div className='chatBox'>
+                {chatLogs}
+            </div>
+
+            <textarea value={userTextInput} onChange={(event) => setUserTextInput(event.target.value)} onKeyDown={submissionHandler} className='userInput'></textarea>
         </div>
     )
 }
