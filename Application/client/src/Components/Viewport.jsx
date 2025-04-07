fb3605d429478ac189e6c38b006e81ba4d2383fb
Abdullah Khan <100245127+106abd@users.noreply.github.com>
2025-04-07 17:53:24 -0400

    Initialize Viewport State

--- Application/client/src/Components/Viewport.jsx
+++ Application/client/src/Components/Viewport.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Viewport() {
+
+    const [htmlResumeCode, setHtmlResumeCode] = useState(`
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <title>Test Page</title>
+                <style>
+                    body { font-family: Arial, sans-serif; background: #f4f4f4; }
+                    header { background: #333; color: white; padding: 10px; }
+                </style>
+            </head>
+            <body>
+                <header>
+                </header>
+                <p>This is a full HTML document rendered in an iframe.</p>
+                <h1>For Real?</h1>
+            </body>
+        </html>
+    `)
+
     return (
         <div className='boxContainer' id='viewportContainer'>
-            <iframe className='viewportRender'>
-            </iframe>
+            <iframe className='viewportRender' srcDoc={htmlResumeCode} />
         </div>
     )
 }
